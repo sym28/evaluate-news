ec8b9efc7fe9a6e7fcec771de11cb16d5bf1a05f
sym28 <29101705+sym28@users.noreply.github.com>
2020-10-28 20:31:49 +0000

    added workbox plugin

---
+++ __test__/nameChecker.test.js
@@ -0,0 +1,9 @@
+import {checkForName} from '../src/client/js/nameChecker'
+
+describe('testing the name checker functionality', () => {
+    
+    test('testing the checkForName() function ', () => {
+        expect(checkForName).toBeDefined();
+    })
+    
+})
--- src/server/mockAPI.js
+++ src/server/mockAPI.js
@@ -1,15 +1,7 @@
-let string1 = 'https://api.meaningcloud.com/sentiment-2.1?key='
-let apiKey = process.env.API_KEY
-let outputFormat = '&of=json'
-let searchText = '&txt=Main%20dishes%20were%20quite%20good%2C%20but%20desserts%20were%20too%20sweet%20for%20me.'
-let model = '&model=general&lang=en'
 
-let fetchCall = string1 + apiKey + outputFormat + searchText + model
 
 let textApi = {
-    api_key: process.env.API_KEY,
-    api_call: fetchCall
-
+    api_key: process.env.API_KEY
 }
 
 
