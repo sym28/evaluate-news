500f6a6a1cbd723a6fec9cc2b35e7862cf630226
sym28 <29101705+sym28@users.noreply.github.com>
2020-10-30 23:11:50 +0000

    added env port

--- src/server/index.js
+++ src/server/index.js
@@ -29,8 +29,9 @@ app.get('/', function (req, res) {
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8081, function () {
-    console.log('app listening on port 8081!')
+const port = process.env.PORT || 8081
+app.listen(port, function () {
+    console.log(`app listening on ${port}`)
 })
 
 // send api response to client
