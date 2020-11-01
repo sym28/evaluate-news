3b3fd3e7fa2eea72e46cff541562dfb03179f92e
sym28 <29101705+sym28@users.noreply.github.com>
2020-11-01 13:40:21 +0000

    changes to server

--- src/server/index.js
+++ src/server/index.js
@@ -24,8 +24,8 @@ app.use(express.static('dist'))
 console.log(__dirname) 
 
 app.get('/', function (req, res) {
-    res.sendFile(path.resolve('dist/index.html'))
-    // res.sendFile(path.resolve('src/client/views/index.html'))
+    // res.sendFile(path.resolve('dist/index.html'))
+    res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
 // designates what port the app will listen to for incoming requests
