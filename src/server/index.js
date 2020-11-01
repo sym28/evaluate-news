9bb7f9407ef41878739be9daa844ec4ee9a8f556
sym28 <29101705+sym28@users.noreply.github.com>
2020-11-01 13:34:32 +0000

    server changes

--- src/server/index.js
+++ src/server/index.js
@@ -24,7 +24,7 @@ app.use(express.static('dist'))
 console.log(__dirname) 
 
 app.get('/', function (req, res) {
-    res.sendFile('dist/index.html')
+    res.sendFile(path.resolve('dist/index.html'))
     // res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
